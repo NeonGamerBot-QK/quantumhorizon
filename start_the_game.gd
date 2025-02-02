8d2f9526e5edcb999be3e9e0dc7aba432610e9fb
Saahil <neon@saahild.com>
2025-02-02 15:52:16 -0500

    feat(tree-wide): init

---
+++ start_the_game.gd
@@ -0,0 +1,19 @@
+extends Sprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _input(event):
+	#print("#event")
+	#print(event is InputEventMouseButton)
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		if get_rect().has_point(to_local(event.position)):  # Check if click is inside sprite
+			print("click ")
+			get_tree().change_scene_to_file("res://main.tscn")
